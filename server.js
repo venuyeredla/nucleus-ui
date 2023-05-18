d60ed7821e42448dc6b0f90bc4e36eb871f74897
Venu Yeredla <venugopal@Venugopals-MacBook-Air.local>
2023-05-18 18:38:58 +0530

    Deleted old file

--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-//setting middleware
-app.use("/",express.static(__dirname)); //Serves resources from public folder
-var server = app.listen(5000);
-console.log("Running application on port :: 5000")
